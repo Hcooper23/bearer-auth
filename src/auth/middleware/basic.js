bcc5b095b1fd8c1dd7dc4f4d7873b4445cc611a1
Hayden James Cooper <haydencpr95@gmail.com>
2023-05-23 22:25:50 -0400

    Lab Class 07

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -1,21 +1,25 @@
 'use strict';
 
 const base64 = require('base-64');
-const { user } = require('../models/index.js');
+const { users } = require('../models/index.js');
 
 module.exports = async (req, res, next) => {
-
-  if (!req.headers.authorization) { return _authError(); }
-
-  let basic = req.headers.authorization;
-  let [username, pass] = base64.decode(basic).split(':');
-
-  try {
-    req.user = await user.authenticateBasic(username, pass)
-    next();
-  } catch (e) {
-    console.error(e);
-    res.status(403).send('Invalid Login');
+  if (!req.headers.authorization) {
+    return _authError(res);
   }
 
+  let basic = req.headers.authorization.split(' ').pop();
+  let [username, password] = base64.decode(basic).split(':');
+
+  try {
+    req.user = await users.authenticateBasic(username, password);
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(403).send('Invalid Login');
+  }
+};
+
+function _authError(res) {
+  res.status(401).send('Unauthorized');
 }
--- src/auth/middleware/bearer.js
+++ src/auth/middleware/bearer.js
@@ -1,21 +1,30 @@
 'use strict';
 
+const jwt = require('jsonwebtoken');
 const { users } = require('../models/index.js');
 
 module.exports = async (req, res, next) => {
-
   try {
-
-    if (!req.headers.authorization) { next('Invalid Login') }
+    if (!req.headers.authorization) {
+      throw new Error('Invalid Login');
+    }
 
     const token = req.headers.authorization.split(' ').pop();
-    const validUser = await users.authenticateWithToken(token);
+    const decodedToken = jwt.verify(token, process.env.SECRET); // Replace 'process.env.SECRET' with the environment variable holding your actual secret key used for signing the tokens
+
+    const username = decodedToken.username;
+    const validUser = await users.findOne({ where: { username } });
+
+    if (!validUser) {
+      throw new Error('Invalid Login');
+    }
 
     req.user = validUser;
-    req.token = validUser.token;
+    req.token = token;
 
+    next();
   } catch (e) {
     console.error(e);
     res.status(403).send('Invalid Login');
   }
-}
+};
